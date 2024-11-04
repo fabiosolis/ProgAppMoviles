b65efe2e86474b6c552d2a77b8dae0ee437a3f0b
fabiosolis <162714037+fabiosolis@users.noreply.github.com>
2024-11-04 06:06:55 -0300

    Add files via upload

--- SkeletonApp/src/app/app-routing.module.ts
+++ SkeletonApp/src/app/app-routing.module.ts
@@ -1,29 +1,22 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
+  { path: 'registro', loadChildren: () => import('./registro/registro.module').then(m => m.RegistroPageModule) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardPageModule) },
+  { path: 'perfil-estudiante', loadChildren: () => import('./perfil-estudiante/perfil-estudiante.module').then(m => m.PerfilEstudiantePageModule) },
+  { path: 'retroalimentacion', loadChildren: () => import('./retroalimentacion/retroalimentacion.module').then(m => m.RetroalimentacionPageModule) },
+  { path: 'configuracion', loadChildren: () => import('./configuracion/configuracion.module').then(m => m.ConfiguracionPageModule) },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'proximas-clases',
+    loadChildren: () => import('./proximas-clases/proximas-clases.module').then( m => m.ProximasClasesPageModule)
   },
-
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- SkeletonApp/src/app/app.module.ts
+++ SkeletonApp/src/app/app.module.ts
@@ -8,10 +8,21 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http'
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localeEs);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }, // Establece el locale por defecto a español
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy } // Mantiene la configuración de IonicRouteStrategy
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
